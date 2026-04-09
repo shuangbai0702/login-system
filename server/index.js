b1569400b71f6c646f400d96401c262117363341
shuangbai0702 <768687099@qq.com>
2026-04-09 22:44:24 +0000

    fix: replace Express 5 incompatible wildcard route with middleware

--- server/index.js
+++ server/index.js
@@ -98,7 +98,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // 所有其他路由返回前端页面
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
 });
 
